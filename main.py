dcf4867220328461fcb75df0b256908814c57ecb
kirisaki-momotaro <christosioannidiskun@gmail.com>
2023-04-10 19:13:25 +0300

    started the code for real

--- main.py
+++ main.py
@@ -7,37 +7,43 @@ experts_list=[]
 ## class that defines a expert and its basic functions
 class expert:  
   ##constructor with error list as input
-  def __init__(self,err):
-    self.err = err
-    
-  ## midle value of the bounds
-  def mu_i(self):
-    return (self.a_i+self.b_i)/2
-  ## returns random value between the bounds
+  
+  def __init__(self,value_array):
+    self.value_array = value_array
+    self.weight=1  
+  
+  def get_weight():
+    return self.weight
+  ## returns error value of expert at a given time 
   def get_value(i):
-    return self.err[i]
+    return self.value_array[i]
   ## a hello message from our dear Mr. expert
   def print_info(self):
-    print(f"Hi im a expert with a_i={self.a_i},b_i={self.b_i} and mu_i = {self.mu_i()}  yoroshiku!!.")
+    print(f"Hi im a expert with ={self.weight} yoroshiku!!.")
+
+
+
+
+    
 
 
 ## create the bounds for each expert
 def init_expert():
-  for i in range(0,29):
-    new_bound1 ,new_bound2 =random.uniform(0, 1) ,random.uniform(0, 1)
-    ## inserts bounds in the correct order smaller->bigger
-    if(new_bound1>new_bound2):
-      experts_list.append(expert(new_bound2,new_bound1))
-    else:
-      experts_list.append(expert(new_bound1,new_bound2))
-
+  expert_value_array = []
+  with open("Milano_timeseries.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    for row in csvreader:
+        expert_value_array.append(row)
+  
+  for i in range(0,29):   
+    experts_list.append(expert(expert_value_array[i]))
     experts_list[i].print_info()
 
 ## returns index of expert with best mui
 def find_optimal_expert_index():
     max_value=0
     max_index=-1
-    for i in range(k):
+    for i in range(30):
       m=experts_list[i].mu_i()
       if(m>max_value):
         max_value=m
@@ -48,7 +54,7 @@ def find_optimal_expert_index():
 def main():
   
   init_expert()
-  optimal_MAB_index= find_optimal_expert_index() #index of the arm with best returns
+ # optimal_MAB_index= find_optimal_expert_index() #index of the arm with best returns
   
   
   
@@ -59,11 +65,4 @@ if __name__=="__main__":
    main()
 
 
-experts = []
-with open("Milano_timeseries.csv", 'r') as file:
-    csvreader = csv.reader(file)
-    for row in csvreader:
-        experts.append(row)
-
-print(experts[1][2])
 
